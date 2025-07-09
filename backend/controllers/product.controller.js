851e51d146f98e9795a8ada6dc37179c54b478a4
rasathelamedude <rasyar.safen19@gmail.com>
2025-07-09 18:09:17 +0300

    Storing and retrieving featured products to/from cache

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -1,4 +1,5 @@
 import Product from "../models/product.model.js";
+import { redis } from "../lib/redis.js";
 
 export const getAllProducts = async (req, res) => {
   try {
@@ -21,8 +22,35 @@ export const getAllProducts = async (req, res) => {
 
 export const getFeaturedProducts = async (req, res) => {
   try {
-    const featuredProducts = await Product.find({ isFeatured: true });
+    // 1. check if we have them cached;
+    let featuredProducts = await redis.get("featured_products");
 
+    if (featuredProducts) {
+      return res.status(200).json({
+        success: true,
+        message: "Fetched featured products",
+        result: featuredProducts.length,
+        data: {
+          featuredProducts: JSON.parse(featuredProducts),
+        },
+      });
+    }
+
+    // Else fetch them from the database;
+    featuredProducts = await Product.find({ isFeatured: true }).lean();
+
+    // check if we have any featured products;
+    if (!featuredProducts) {
+      return res.status(404).json({
+        success: false,
+        message: "No featured products found in the database!",
+      });
+    }
+
+    // After fetching it from the database, store it in the cache;
+    await redis.set("featured_products", JSON.stringify(featuredProducts));
+
+    // send back response;
     res.status(200).json({
       success: true,
       message: "Fetched featured products",
