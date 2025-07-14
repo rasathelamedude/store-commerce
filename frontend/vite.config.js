dcb9026ab06dd72a714cf46582017950e0aea011
rasathelamedude <rasyar.safen19@gmail.com>
2025-07-14 16:20:59 +0300

    Set up frontend structure and boilerplate

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    Proxy: {
-      "/api": {
-        target: "http://localhost:5110",
-        changeOrigin: true,
-      },
-    },
-  },
+  plugins: [react(), tailwindcss()],
 });
